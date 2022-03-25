05c81501f18023fa0501819ab9709f521b05c1e3
vivian31aa <vivian31aa@gmail.com>
2022-03-25 09:57:21 +0800

    two city practice

---
+++ 1029. Two City Scheduling.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int total=0;
+        vector<int> diff;
+        
+        // Total cost to city a.
+        for(int i=0;i<costs.size();i++){
+            total+=costs[i][0];
+        }
+        
+        // Cost difference if go to city b.
+        for(int i=0;i<costs.size();i++){
+            diff.push_back(costs[i][1]-costs[i][0]);
+        }
+
+        // Add the minimum cost difference.
+        sort(diff.begin(),diff.end());
+        for(int i=0;i<costs.size()/2;i++){
+            total+=diff[i];
+        }
+        return total;
+    }
+};
